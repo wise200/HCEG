5cee5043e71f51a3a8579aa81ab57827f6023c30
wise200 <jrwise200@gmail.com>
2020-03-27 23:15:19 -0500

    added login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -14,14 +15,38 @@ def apply():
             'banner_img': 'apply.jpg'}
     return render_template('apply.html', info=info)
 
+@app.route('/clients')
+def clients():
+    info = {'title': 'CLIENTS',
+            'homepage': False,
+            'banner_img': 'clients.jpg'}
+    return render_template('clients.html', info=info)
+
+@app.route('/analysts/overview')
+def analysts_overview():
+    info = {'title': 'OVERVIEW',
+            'homepage': False,
+            'banner_img': 'apply.jpg'}
+    return render_template('analysts_overview.html', info=info)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('Login requested for user {} with pass {}'.format(form.username.data, form.password.data))
+        return redirect(url_for('index'))
+    info = {'title': 'DIVINE VERIFICATION',
+            'homepage': False,
+            'banner_img': 'eye_of_providence.jpg',
+            'form': form}
+    return render_template('login.html', info=info)
+
 @app.route('/consultforacauseresults19')
 def charity():
     return redirect('https://harvardcbe.com')
 
 # dead links
 @app.route('/about')
-@app.route('/clients')
-@app.route('/analysts/overview')
 @app.route('/analysts/application')
 @app.route('/analysts/experience')
 @app.route('/analysts/community')
